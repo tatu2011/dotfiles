4b8d1c13698917cad1a66e7b4f7d94ba153b994a
tatu2011 <117667919+tatu2011@users.noreply.github.com>
2024-07-06 13:04:23 +0500

    Update select_wallpaper.sh

--- hypr/scripts/select_wallpaper.sh
+++ hypr/scripts/select_wallpaper.sh
@@ -14,7 +14,7 @@ if [ -n "$SELECTED_WALLPAPER" ]; then
     echo "Setting wallpaper with swww: $SELECTED_WALLPAPER"
     
     # Установка обоев с помощью swww
-    swww img "$SELECTED_WALLPAPER"
+    swww img "$SELECTED_WALLPAPER" 
     if [ $? -ne 0 ]; then
         echo "Failed to set wallpaper with swww."
         exit 1
@@ -35,9 +35,30 @@ if [ -n "$SELECTED_WALLPAPER" ]; then
         echo "Failed to create Telegram theme with wal-telegram."
         exit 1
     fi
+
+    # Проверка наличия файла colors-waybar.css
+    if [ ! -f /home/tatu/.cache/wal/colors-waybar.css ]; then
+        echo "colors-waybar.css not found."
+        exit 1
+    fi
+
+    # Извлечение цвета @color3 из colors-waybar.css
+    COLOR3_HEX=$(grep '@define-color color3' /home/tatu/.cache/wal/colors-waybar.css | awk '{print $3}' | tr -d ';')
     
+    if [ -z "$COLOR3_HEX" ]; then
+        echo "Failed to extract @color3 from colors-waybar.css."
+        exit 1
+    fi
+
+    echo "Extracted @color3: $COLOR3_HEX"
+
+    # Удаление символа # из значения цвета
+    COLOR3_HEX_NO_HASH=$(echo "$COLOR3_HEX" | tr -d '#')
+
+    # Сохранение цвета в файл для использования в конфигурации Hyprland
+    echo "\$border_color_active=$COLOR3_HEX_NO_HASH" > /home/tatu/.config/hypr/color_active_border
+    echo "Saved active border color to /home/tatu/.config/hypr/color_active_border"
     echo "All operations completed successfully."
 else
     echo "No wallpaper selected or failed to get wallpaper path from waypaper."
 fi
-
